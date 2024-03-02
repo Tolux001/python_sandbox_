a4e57ed6a7da650f44580167c6998daa70db5649
Tolux001 <toluwalasepeter100@gmail.com>
2024-03-02 23:55:28 +0100

    Learnt Variable

--- basic.py
+++ basic.py
@@ -20,10 +20,23 @@
 # # Using type : to know the data type used
 # print(type(name))
 
-# int 
-age = 21
-# age = age + 1
-age += 1
+# # int 
+# age = 21
+# # age = age + 1
+# age += 1
 
-# print(age)
-print(f'Your name is {age}')
+# # print(age)
+# # print(type(age))
+# print(f'Your name is {age}')
+
+# float
+height = 259.39
+# print(height)
+# print(type(height))
+print(f'Your are {height}cm tall. The work of Beans isn your life')
+
+# Boolean
+Animal = True
+Human = False
+print(type(Animal))
+print(f'Are you human: {Human}, or are you an animal: {Animal}')
