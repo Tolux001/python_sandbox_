4ff7f4c987df7b1f5fbe8fe7350caca8b8ed0fc6
Tolux001 <toluwalasepeter100@gmail.com>
2024-04-13 14:45:13 +0100

    functions

--- functions.py
+++ functions.py
@@ -18,15 +18,19 @@ print(value)
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
-# From running it, I think lambda is a single line function, i tried making it multiple lines but their was error in the first line
+# PS: From running it, I think lambda is a single line function, i tried making it multiple lines but their was error in the first line
 # I would do my research though
 getDifference = lambda num1, num2: num1 - num2
 sub_value = getDifference(100, 105)
 print(sub_value)
 
+arithmetics = lambda test, test1, test2: test + test1 - test2
+value = arithmetics(48, 20, 60)
+print(value)
+
 #Getting used to writing functions
 def military_uniform(rank, age, service_time):
-    return f'You are a {rank} so respect people older especially when you are a {age} year old with not more than {service_time}'
+    return f'You are a {rank} so respect people older especially when you are a {age} year old with not more than {service_time} years of service time'
 military_uniform2 = lambda rank, age, service_time: f'You are a {rank} so respect people older especially when you are a {age} year old with not more than {service_time}'
 
 print(military_uniform("Corporal", 23, 9))
