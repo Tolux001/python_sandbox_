5c9ad57ac2a7bd3b8024c9abbb9221d261f1dbc3
Tolux001 <toluwalasepeter100@gmail.com>
2024-04-14 18:32:07 +0100

    Modules

--- modules.py
+++ modules.py
@@ -1,47 +1,47 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
 
-# # Core modules
+# Core modules
 
-# import datetime
-# today = datetime.date.today()
+import datetime
+today = datetime.date.today()
 
-# print(today)
+print(today)
 
-# from datetime import date
-# today = date.today()
+from datetime import date
+today = date.today()
 
-# print(today)
+print(today)
 
-# import time
-# timeStamp = time.time()
+import time
+timeStamp = time.time()
 
-# print(timeStamp)
+print(timeStamp)
 
-# from time import time 
-# timeStamp = time()
+from time import time 
+timeStamp = time()
 
-# print(timeStamp)
+print(timeStamp)
 
-# #Pip modules
-# from camelcase import CamelCase
+#Pip modules
+from camelcase import CamelCase
 
-# c = CamelCase()
-# answer = c.hump("hello world")
-# print(answer)
+c = CamelCase()
+answer = c.hump("hello world")
+print(answer)
 
-#Import custom: Importing a file that you have 
-import validator
-from validator import validate_email
-from validator import nameValidator
+# #Import custom: Importing a file that you have 
+# import validator
+# from validator import validate_email
+# from validator import nameValidator
 
-# Email_validator
-email = 'tolux001@yahoo.com'
-if validate_email(email):
-    print("Email is valid")
-else:
-    print("Email is bad")
+# # Email_validator
+# email = 'tolux001@yahoo.com'
+# if validate_email(email):
+#     print("Email is valid")
+# else:
+#     print("Email is bad")
 
-# Name_validator
-name1 = "Tolux001"
-name2 = "Tolux001"
-print(nameValidator(name1, name2))
+# # Name_validator
+# name1 = "Tolux001"
+# name2 = "Tolux001"
+# print(nameValidator(name1, name2))
