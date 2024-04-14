f034792b850123658b79ce8f0b2bb75d6ba68a40
Tolux001 <toluwalasepeter100@gmail.com>
2024-04-14 18:58:49 +0100

    Classes

--- classes.py
+++ classes.py
@@ -15,7 +15,7 @@
 # In python
 class Animal:
     # constructor
-    def init(self, name, family, genres, specie):
+    def __init__(self, name, family, genres, specie):
         self.name = name
         self.family = family
         self.genres = genres
@@ -23,3 +23,22 @@ class Animal:
 parrot = Animal('Emmanuel', 'Biology_children_answer_this', 'one', 'two')
 
 print(type(parrot))
+
+# Classes
+class Self:
+    def __init__(self, name, age, email, stack, github):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.stack = stack
+        self.github = github
+    def my_specs(self):
+        return f'My name is {self.name} and I am currently {self.age}. Send opportunities my way via {self.email}, currently a {self.stack}; and yes {self.github} github account is mine'
+    
+Tolux = Self('Toluwalase', 22, 'toluwalasepeter001@gmail.com', 'MERN', 'tolux001')
+
+print(Tolux.my_specs())
+
+# Using Extends
+
+# Ps: Make sure '__init__' and 'self' to call parameter is used in the constructor
