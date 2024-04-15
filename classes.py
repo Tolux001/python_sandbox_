96328bf5f460c28db2b8871921dfdfbd8593980a
Tolux001 <toluwalasepeter100@gmail.com>
2024-04-15 12:06:44 +0100

    Classes

--- classes.py
+++ classes.py
@@ -13,6 +13,9 @@
 
 
 # In python
+from copyreg import constructor
+
+
 class Animal:
     # constructor
     def __init__(self, name, family, genres, specie):
@@ -33,12 +36,48 @@ class Self:
         self.stack = stack
         self.github = github
     def my_specs(self):
-        return f'My name is {self.name} and I am currently {self.age}. Send opportunities my way via {self.email}, currently a {self.stack}; and yes {self.github} github account is mine'
+        return f'My name is {self.name} and I am currently {self.age}. Send opportunities my way via {self.email}, currently achieving the {self.stack} stack to my platter; and yes {self.github} github account is mine'
     
 Tolux = Self('Toluwalase', 22, 'toluwalasepeter001@gmail.com', 'MERN', 'tolux001')
 
 print(Tolux.my_specs())
 
 # Using Extends
+
+class Universities:
+    # constructor
+    def __init__(self, test1, test2, test3):
+        self.test1 = test1
+        self.test2 = test2
+        self.test3 = test3
+    # Methods
+    def test_score(self):
+        self.test1 += self.test2
+        return self.test1
+    def department(self):
+        return self.test3
+
+# Extending universities
+class Funaab(Universities):
+    # constructor
+    def __init__(self, test1, test2, test3, college):
+        self.test1 = test1
+        self.test2 = test2
+        self.test3 = test3
+        self.college = college
+    def department(self):
+        return f'The {self.college} is current the best after being awarded {self.test1} medals of honor'
+    
+    # setting a parameter outside the constructor and inside a function
+    def set_balance(self, balance):
+        self.balance = balance
+        return f'You currently have {self.balance} in your wallet'
+    
+# Accessing methods in the extended(super) class and overwriting them
+test = Funaab(50, 130, 'Computer_Science', 'College of physical science')
+
+print(test.test_score()) # Accessing a method in the parent class || Updating the value of test1
+print(test.department())
+print(test.set_balance(300))
 
 # Ps: Make sure '__init__' and 'self' to call parameter is used in the constructor
